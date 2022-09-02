b8b55c4e301b32524afe820c51092b194b89bae1
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-02 19:04:22 +0600

    modal added

--- js/hunting.js
+++ js/hunting.js
@@ -26,6 +26,30 @@ let getPhones = (phoneData) => {
         <p class="card-text">
         This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
         </p>
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Show Details
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>
         </div>
         `;
